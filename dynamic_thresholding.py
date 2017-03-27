0b9dbaa0603b765bb06ac96023b77ebb1e2344c8
gautamits <gautamamits95@gmail.com>
2017-03-27 23:37:48 +0530

    added comments

--- dynamic_thresholding.py
+++ dynamic_thresholding.py
@@ -8,34 +8,40 @@ import easygui
 import sys
 from binary import get_binary
 def print_value(val):
+	#read values from lower and upper slider
 	l=[]
 	for i in lowers:
 		l.append(i.get())
 	u=[]
 	for i in uppers:
 		u.append(i.get())
+	#call binary function
 	binary=get_binary((gray,l[0],u[0]))
 	im = PIL.Image.fromarray(binary)
 	imgtk = PIL.ImageTk.PhotoImage(image=im)
+	#update image of page 3
 	l1.configure(image=imgtk)
 	l1.image=imgtk
-
+#read image 
 img = cv2.imread(sys.argv[1])
 height,width=img.shape[:2]
+#resize image
 ratio=float(700)/height
 img=cv2.resize(img,(int(ratio*width),int(ratio*height)))
-
+#convert t0 grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 thresholds=raw_input("enter names of all thresholds. eg R G B : ").split()
 
 tk=Tk()
-
+#page one will contain all sliders for lower value
 page1 = Frame(tk,bg="blue",width=1200,height=100)
 lowers=[0]*len(thresholds)
 for i in range(len(thresholds)):
 	lowers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value,label=thresholds[i]+"lower")
 	lowers[i].set(0)
 	lowers[i].pack(side=LEFT,expand=1,fill=BOTH)
+	
+#page2 will contain all sliders with upper value
 page2 = Frame(tk,bg="blue",width=1200,height=100)
 
 uppers=[0]*len(thresholds)
@@ -43,7 +49,7 @@ for i in range(len(thresholds)):
 	uppers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value,label=thresholds[i]+"upper")
 	uppers[i].set(255)
 	uppers[i].pack(side=LEFT,expand=1,fill=BOTH)
-
+#page 3 will show the image 
 page3 = Frame(tk,bg="blue",width=1200,height=700)
 im = PIL.Image.fromarray(gray)
 imgtk = PIL.ImageTk.PhotoImage(image=im)

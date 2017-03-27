063261c54928ab0a4ac32e86c499bd9451ce7b3d
gautamits <gautamamits95@gmail.com>
2017-03-27 23:26:07 +0530

    hard coded for binary.py

--- dynamic_thresholding.py
+++ dynamic_thresholding.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageTk
 import numpy as np
 import easygui
 import sys
+from binary import get_binary
 def print_value(val):
 	l=[]
 	for i in lowers:
@@ -25,42 +26,21 @@ ratio=float(700)/height
 img=cv2.resize(img,(int(ratio*width),int(ratio*height)))
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#Rearrang the color channel
-#b,g,r = cv2.split(hsv)
-#img = cv2.merge((r,g,b))
-thresholds=raw_input("enter names of all thresholds. eg R G B ").split()
-from binary import get_binary
+thresholds=raw_input("enter names of all thresholds. eg R G B : ").split()
+
 tk=Tk()
 
 page1 = Frame(tk,bg="blue",width=1200,height=100)
-#lr = Scale(page1,orient='horizontal', from_=0, to=255, command=print_value)
-#lg = Scale(page1,orient='horizontal', from_=0, to=255, command=print_value)
-#lb = Scale(page1,orient='horizontal', from_=0, to=255, command=print_value)
-#lr.set(40)
-#lg.set(0)
-#lb.set(0)
-#lr.pack(side=LEFT,expand=1,fill=BOTH)
-#lg.pack(side=LEFT,expand=1,fill=BOTH)
-#lb.pack(side=LEFT,expand=1,fill=BOTH)
 lowers=[0]*len(thresholds)
 for i in range(len(thresholds)):
-	lowers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value)
+	lowers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value,label=thresholds[i]+"lower")
 	lowers[i].set(0)
 	lowers[i].pack(side=LEFT,expand=1,fill=BOTH)
-
 page2 = Frame(tk,bg="blue",width=1200,height=100)
-#hr = Scale(page2,orient='horizontal', from_=0, to=255, command=print_value)
-#hg = Scale(page2,orient='horizontal', from_=0, to=255, command=print_value)
-#hb = Scale(page2,orient='horizontal', from_=0, to=255, command=print_value)
-#hr.set(255)
-#hg.set(40)
-#hb.set(255)
-#hr.pack(side=LEFT,expand=1,fill=BOTH)
-#hg.pack(side=LEFT,expand=1,fill=BOTH)
-#hb.pack(side=LEFT,expand=1,fill=BOTH)
+
 uppers=[0]*len(thresholds)
 for i in range(len(thresholds)):
-	uppers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value)
+	uppers[i]=Scale(page1,orient='horizontal', from_=0, to=255, command=print_value,label=thresholds[i]+"upper")
 	uppers[i].set(255)
 	uppers[i].pack(side=LEFT,expand=1,fill=BOTH)
 
